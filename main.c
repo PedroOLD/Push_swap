2e13125e35576d0df7728aa01ac1837681779c57
Pedro Old <pedrohenrique.santos@corabiz.ag>
2026-02-22 23:44:03 -0300

    feat/aqui man

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/27 11:52:10 by marvin            #+#    #+#             */
-/*   Updated: 2026/02/16 19:47:18 by marvin           ###   ########.fr       */
+/*   Updated: 2026/02/22 23:33:25 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -60,10 +60,13 @@ static int	isvalid_arg(char *arg)
 
 int main(int ac, char **av) {
 	int 			i;
+	int				j;
 	int				current_number;
+	char			**matrix_str_number;
 	t_stack			*node;
 	static t_stack	*stack_a;
 	// static t_stack	*stack_b;
+	t_stack			*temp;
 
 	i = 1;
 	if (ac == 1)
@@ -72,21 +75,27 @@ int main(int ac, char **av) {
 	{
 		if (isvalid_arg(av[i]))
 		{
-			current_number = ft_atoi(av[i]);
-			node = new_node(current_number);
-			add_front_list(&stack_a, node);
-			free(node);
+			j = 0;
+			matrix_str_number = ft_split(av[i], ' ');
+			while (matrix_str_number[j] != NULL)
+			{
+				current_number = ft_atoi(matrix_str_number[j]);
+				node = new_node(current_number);
+				add_front_list(&stack_a, node);
+				j++;
+			}
 		}
 		else
 			return (0); 
 		i++;
 	}
 
-	while (stack_a != NULL)
+	temp = stack_a;
+	while (temp != NULL)
 	{
-		printf("aqui (%i)\n", stack_a->number);
+		printf("aqui (%i)\n", temp->number);
 
-		stack_a = stack_a->next;
+		temp = temp->next;
 	}
 	return (0);
 }
